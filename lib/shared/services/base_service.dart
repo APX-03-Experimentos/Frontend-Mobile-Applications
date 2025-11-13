500eb52358105d089c07b9b11885d213c47d20fe
Firtness <u202122484@upc.edu.pe>
2025-11-13 13:45:00 -0500

    feat(baseUrl): added baseUrl

--- lib/shared/services/base_service.dart
+++ lib/shared/services/base_service.dart
@@ -1,6 +1,6 @@
 class BaseService {
 
-  final String baseUrl = "https://backend-web-services-1.onrender.com";
+  final String baseUrl = "https://backend-web-services-1.onrender.com/api/v1/";
 
   final String endpoint;
 
